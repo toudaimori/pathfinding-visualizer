93ded70b3c70c7d22ea05972cea7f97f12dfca3e
toudaimori <commmishima@gmail.com>
2026-02-14 03:23:36 +0900

    chore: setup deployment with GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // v4の場合
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/pathfinding-visualizer/', // ★ここをあなた自身のリポジトリ名に変更してください
 })
